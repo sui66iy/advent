ca4d9a83d16c03d648648ddf89da07e2dbdadcef
Michael Higgins <michael.a.higgins@nielsen.com>
2018-12-06 11:48:03 -0500

    Comments.

--- day5/react.clj
+++ day5/react.clj
@@ -9,6 +9,8 @@
 (require '[clojure.core.rrb-vector :as fv])
 
 (defn ^boolean reactable?
+  "Do these two characters 'react'?  E.g., Aa does, aA does,
+  Ab does not, nor does AA."
   [^Character x
    ^Character y]
   (and (not (= x y))
@@ -16,6 +18,10 @@
           (string/lower-case y))))
 
 (defn react
+  "Find a reactable pair of characters in the vector s and remove them,
+  starting your search at start-idx.
+
+  Return the new vector and an index at which to start your next search."
   [s start-idx]
   (loop
       [front (fv/subvec s 0 (+ start-idx 1))
@@ -35,6 +41,10 @@
                (+ idx 1))))))
 
 (defn react-until-fixed
+  "Keep removing reactable pairs of characters from the vector s until
+  there no such pairs.
+  Return the resulting vector (with all reactable pairs removed).
+  tag is a string to include when printing status."  
   [s tag]
   (loop
       [prior s
@@ -42,31 +52,34 @@
        i 1]
     (let
         [[current new-index] (react prior start-index)]
-      (if (= (mod i 100) 0)
-        (println (str tag (count current)))
-        nil)
+      (when (= (mod i 100) 0)
+        (println tag (count current))) ; print out some progress periodically!
       (if (= (count prior) (count current))
         current
-        (recur current new-index (+ i 1))))))
+       (recur current new-index (+ i 1))))))
 
 (defn remove-unit
+  "Given a lower-case character, removes all instances of that character and its
+  upper-case variant from the vector s, and returns the new vector."
   [unit s]
-  (filter #(and (not (= % unit))
-                (not (= % (first (string/upper-case unit)))))
-          s))
+  (into []
+        (filter #(and (not (= % unit))
+                      (not (= % (first (string/upper-case unit)))))
+                s)))
 
 (defn optimal-removal
+  "Find the shortest possible vector obtained after fully reacting every
+  possible vector obtained by removing one type of character (unit).
+  Return the length of that vector and the unit removed to obtain it."
   [s]
-  (let [units (set (map #(first (string/lower-case %)) s))]
-    (first
-     (sort
-      (pmap
-       (fn [unit]
-         (println unit)
-         [(count (react-until-fixed (into [] (remove-unit unit s)) unit)) unit])
-       (sort units))))))
+  (first
+   (sort
+    (pmap
+     (fn [unit]
+       [(count (react-until-fixed (remove-unit unit s) unit)) unit])
+     (set (map #(first (string/lower-case %)) s))))))
 
-(def test-main (into [] "dabAcCaCBAcCcaDA"))
+(def test-main (into [] "dabAcCaCBAcCcaDA")) ; example test data
 
 (defn read-file
   [filename]
