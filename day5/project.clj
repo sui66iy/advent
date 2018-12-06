5dd7a2ca6eca857500c055d458282e09e02326c6
Michael Higgins <michael.a.higgins@nielsen.com>
2018-12-06 01:08:57 -0500

    Day 5 solution

---
+++ day5/project.clj
@@ -0,0 +1,6 @@
+
+(defproject day5 "0.1.0-SNAPSHOT"
+  :description "Advent of Code Day 5"
+  :dependencies [[org.clojure/core.rrb-vector "0.0.12"]]
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all}})
--- day5/react.clj
+++ day5/react.clj
@@ -3,46 +3,71 @@
 
 (defn reactable?
   [x y]
-  (and (= (string/lower-case x)
-          (string/lower-case y))
-       (or (and (Character/isUpperCase x)
-                (Character/isLowerCase y))
-           (and (Character/isLowerCase x)
-                (Character/isUpperCase y)))))
+  (and (not (= x y))
+       (= (string/lower-case x)
+          (string/lower-case y))))
 
 (defn react
-  [s]
+  [s start-idx]
   (loop
-      [front (subvec s 0 1)
-       back (subvec s 1)]
+      [front (subvec s 0 (+ start-idx 1))
+       back (subvec s (+ start-idx 1))
+       idx start-idx]
     (let [b (first back)]
       (cond
         (nil? b)
-        front
+        [front (max 0 (- idx 1))]
         (reactable? (last front) b)
-        (into (subvec front 0 (- (count front) 1))
-              (subvec back 1))
+        [(into (subvec front 0 (- (count front) 1))
+               (subvec back 1))
+         (max 0 (- idx 1))]
         :else
         (recur (conj front b)
-               (subvec back 1))))))
+               (subvec back 1)
+               (+ idx 1))))))
 
 (defn react-until-fixed
-  [s]
+  [s tag]
   (loop
-      [prior s]
+      [prior s
+       start-index 0
+       i 1]
     (let
-        [current (react prior)]
-      (println (count current))
+        [[current new-index] (react prior start-index)]
+      (if (= (mod i 100) 0)
+        (println (str tag (count current)))
+        nil)
       (if (= (count prior) (count current))
         current
-        (recur current)))))
-  
+        (recur current new-index (+ i 1))))))
+
+(defn remove-unit
+  [unit s]
+  (filter #(and (not (= % unit))
+                (not (= % (first (string/upper-case unit)))))
+          s))
+
+(defn optimal-removal
+  [s]
+  (let [units (set (map #(first (string/lower-case %)) s))]
+    (first
+     (sort
+      (pmap
+       (fn [unit]
+         (println unit)
+         [(count (react-until-fixed (into [] (remove-unit unit s)) unit)) unit])
+       (sort units))))))
+
 (def test-main (into [] "dabAcCaCBAcCcaDA"))
 
 (defn read-file
   [filename]
-  (into [] (slurp filename)))
+  (into [] (string/trim-newline (slurp filename))))
 
 (defn main
   [filename]
-  (count (react-until-fixed (read-file filename))))
+  (count (react-until-fixed (read-file filename) "")))
+
+(defn main2
+  [filename]
+  (optimal-removal (read-file filename)))
